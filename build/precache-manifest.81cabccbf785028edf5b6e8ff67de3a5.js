662d3eefa3746f1cdf6d8dd8984d0f31b5c08c64
aleksi <a.aleksi.tarvainen@gmail.com>
2019-05-28 13:22:26 +0300

    Frontti mukaan oikeesti

---
+++ build/precache-manifest.81cabccbf785028edf5b6e8ff67de3a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc860331135e05d3e4fe0be644652f34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f35b9bfeaa7cbc905fa",
+    "url": "/static/css/main.dbbd9661.chunk.css"
+  },
+  {
+    "revision": "37cfae212e25180f959b",
+    "url": "/static/js/2.0b74813f.chunk.js"
+  },
+  {
+    "revision": "8f35b9bfeaa7cbc905fa",
+    "url": "/static/js/main.26d43030.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
